6ca9957a0fd87300fb35bec6f6dc0bc4e2b44f7b
bga <bga.email@gmail.com>
2020-11-22 17:44:10 +0300

    + accept optional profile name

--- show-size.bat
+++ show-size.bat
@@ -1,1 +1,3 @@
-tail -n 6 "%~dp0.\Debug\List\stm8-volt-amper-meter-iar.map" | head -n 3
+set buildProfile=Release
+if not _%1==_ set buildProfile=%~1
+tail -n 6 "%~dp0.\%buildProfile%\List\stm8-volt-amper-meter-iar.map" | head -n 3
