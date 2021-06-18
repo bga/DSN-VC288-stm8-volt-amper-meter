7f6e30d7bd6e0c11052c4130b72cb314adebf43c
bga <bga.email@gmail.com>
2021-06-19 00:52:53 +0300

    new implementation. use iar-stm8_dump-section.sh

--- tools/iar-stm8_dump-flash.sh
+++ tools/iar-stm8_dump-flash.sh
@@ -1,13 +1,5 @@
 IN_F=$1
 OUT_F=${2:-$IN_F-flash.bin} 
 echo "$OUT_F" 
-SECTION='P3-P5 ro'
-
-#  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al
-# ...
-# [ 7] P3-P5 ro          PROGBITS        00008080 0000b4 00015b 01  AX  0   0  1
-readelf -S "$IN_F" |
-	grep "$SECTION" |
-	awk -F 'PROGBITS' '{print $2}' |
-	awk '{print "dd if='$IN_F' of='$OUT_F' bs=1 skip=$[0x" $2 "] count=$[0x" $3 "]"}' |
-	bash
+selfDir=$(dirname "$0")
+("$selfDir/iar-stm8_dump-section.sh" "$IN_F" 'PROGBITS' 'A2 rw'; "$selfDir/iar-stm8_dump-section.sh" "$IN_F" 'PROGBITS' 'P3-P5 ro') > "$OUT_F"
